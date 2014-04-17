11fdc8243356b28066f9f782f1041d0dfe360f35
Alittlemurkling <Alittlemurkling@github.com>
2014-04-17 16:26:41 -0400

    commit

--- src/dooooom/jmpd/data/Command.java
+++ src/dooooom/jmpd/data/Command.java
@@ -2,6 +2,6 @@ package dooooom.jmpd.data;
 
 public enum Command {
     NULL, TOGGLE, PAUSE, PLAY, STOP, PREV,
-    NEXT, ADD, ADDTOPLAYLIST, REM,
+    NEXT, ADD, ADDTOPLAYLIST, REM, DATA,
     REMPLAYLIST, DEL, ACK
 }
