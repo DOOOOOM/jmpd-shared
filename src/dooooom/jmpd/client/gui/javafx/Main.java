80f1101d300a82629b4fb3568186c560af688a42
Philip Rodning <philip.rodning@gmail.com>
2014-04-26 06:41:06 -0400

    not actually sure what happened here. IDEA seems to have removed some unnecessary imports automatically

--- src/dooooom/jmpd/client/gui/javafx/Main.java
+++ src/dooooom/jmpd/client/gui/javafx/Main.java
@@ -1,13 +1,10 @@
 package dooooom.jmpd.client.gui.javafx;
 
-import dooooom.jmpd.client.UDPClient;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
